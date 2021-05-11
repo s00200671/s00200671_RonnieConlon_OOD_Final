a45135f468751e655ee65f82aa765f66b0e7a54c
redista <sealacquire@gmail.com>
2021-05-11 09:39:18 +0100

    2. Added Game class

---
+++ s00200671_RonnieConlon_OOD_Final/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s00200671_RonnieConlon_OOD_Final
+{
+    class Game
+    {
+        // Properties
+        public string Name { get; set; }
+        public int MetacriticScore { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public decimal Price { get; set; }
+        public string Game_Image { get; set; }
+
+        // Constructors
+        public Game(string name, int metacriticscore, string description, string platform, decimal price,string game_image = "")
+        {
+            Name = name;
+            MetacriticScore = metacriticscore;
+            Description = description;
+            Platform = platform;
+            Price = price;
+            Game_Image = game_image;
+        }
+
+        public void DecreasePrice(decimal amount)
+        {
+            Price -= amount;
+        }
+    }
+}
